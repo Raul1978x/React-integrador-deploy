232fdc2cb72e6a610cfe4b502910de74722b612c
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2023-12-16 11:47:15 -0300

    fix errors

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-3 my-4">
     <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
+      <li className="nav-item"><a className="nav-link px-2 text-body-secondary">Home</a></li>
       {/* <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
       <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
       <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
